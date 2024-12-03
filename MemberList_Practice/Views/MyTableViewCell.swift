9548b9dd9222577b1e6276715e47ee685acdc294
youngjin <bryjna07@nate.com>
2024-12-03 16:26:02 +0900

    feat:테이블뷰 셀 구현

---
+++ MemberList_Practice/Views/MyTableViewCell.swift
@@ -0,0 +1,129 @@
+//
+//  MyTableViewCell.swift
+//  MemberList_Practice
+//
+//  Created by t2023-m0033 on 12/3/24.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+    
+    //MARK: - UI구현
+    
+    let mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let memberNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let addressLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.distribution  = .fill
+        sv.alignment = .fill
+        sv.spacing = 5
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
+    //MARK: - 생성자 셋팅
+    // 코드로 짤때 생성자, 애플이 제공
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        // super.init ⭐️ 저장속성을 세팅을 해주는 것을 , 상위에서 정의한 것은 상위에 다시 위임해줘야함
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        setupStackView()
+        
+        // 셀 오토레이아웃 일반적으로 생성자에서 잡으면 됨 ⭐️⭐️⭐️
+        setConstraints()
+    }
+    
+    func setupStackView() {
+
+        self.contentView.addSubview(mainImageView)
+        self.contentView.addSubview(stackView)
+        
+        // 스택뷰 위에 뷰들 올리기
+        stackView.addArrangedSubview(memberNameLabel)
+        stackView.addArrangedSubview(addressLabel)
+    }
+    
+    // 필수 생성자, 자동으로 가능, 지정생성자를 재정의 할 때 필수 생성자를 구현해줘야함, 자동상속이 안됨
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+      
+       
+    // 스토리보드로 짤 때 사용 - 코드로 짤 땐 없어도됨
+    // viewDidLoad같은 역할, 생성자와 비슷한 역할을 하는 함수
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//             
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+    //MARK: - 오토레이아웃 셋팅
+    // (오토레이아웃 변하지 않는 경우) 일반적으로 생성자에서 잡으면 됨 ⭐️⭐️⭐️
+//    override func updateConstraints() {
+//        setConstraints()
+//        super.updateConstraints()
+//    }
+    //Drawingcycle
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.mainImageView.clipsToBounds = true
+        self.mainImageView.layer.cornerRadius = self.mainImageView.frame.width / 2
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            mainImageView.heightAnchor.constraint(equalToConstant: 40),
+            mainImageView.widthAnchor.constraint(equalToConstant: 40),
+            
+            // self.leadingAnchor로 잡는 것보다 self.contentView.leadingAnchor로 잡는게 더 정확함 ⭐️
+            // (cell은 기본뷰로 contentView를 가지고 있기 때문)
+            mainImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10),
+            mainImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            memberNameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: mainImageView.trailingAnchor, constant: 20),
+            
+            // self.trailingAnchor로 잡는 것보다 self.contentView.trailingAnchor로 잡는게 더 정확함 ⭐️
+            // (cell은 기본뷰로 contentView를 가지고 있기 때문)
+            stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: self.mainImageView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.mainImageView.bottomAnchor)
+        ])
+    }
+    
+    
+    
+    
+    
+}
