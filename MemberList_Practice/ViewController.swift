6eaf7bdc9c1a257f62fae25ddd50e7da0adfe367
youngjin <bryjna07@nate.com>
2024-12-03 13:53:03 +0900

    feat: 테이블 뷰 생성

---
+++ MemberList_Practice/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  MemberList_Practice
+//
+//  Created by t2023-m0033 on 12/3/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    // 테이블 뷰, 굳이 뷰를 따로 나누지 않음
+    private let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        setupTableViewConstraints()
+    }
+
+    // 테이블뷰의 오토레이아웃 설정
+    func setupTableViewConstraints() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+        
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+        
+    }
+    
+    
+    
+}
